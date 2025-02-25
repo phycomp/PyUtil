df5467c463bdcb7f99fba9de3c14093697fa58b1
phycomp <phycomp@gmail.com>
2025-02-25 18:06:07 +0800

    Update movieMNPL.py

--- vid/movieMNPL.py
+++ vid/movieMNPL.py
@@ -1,3 +1,23 @@
+import os
+from moviepy.editor import AudioFileClip, VideoFileClip
+def mnplMovie():
+  org_video_path = input("Enter the video path: ")
+  audio_path = input("Enter the audio path: ")
+  final_video_path = input("Enter the output folder path: ")
+  final_video_name = input("Enter the final video name: ")
+  start_dur = int(input("Enter the starting duration in seconds: "))
+  end_dur = int(input("Enter the ending duration in seconds: "))
+
+  final_video_path = os.path.join(final_video_path, final_video_name)
+
+  video_clip = VideoFileClip(org_video_path)
+
+  background_audio_clip = AudioFileClip(audio_path)
+  bg_music = background_audio_clip.subclip(start_dur, end_dur)
+
+  final_clip = video_clip.set_audio(bg_music)
+  final_clip.write_videofile(final_video_path, codec='libx264', audio_codec="aac")
+
 from stUtil import rndrCode
 
 def mkEnd(oriClip):
